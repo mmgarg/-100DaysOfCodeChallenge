bb4cf023c93ff88e351829d128d54f8fe1c0317f
Madan Mohan Garg <117309374+mmgarg@users.noreply.github.com>
2022-11-15 13:39:34 +0530

    Update Solution.java

--- Day007-Data Types/Solution.java
+++ Day007-Data Types/Solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class solution {
+public class Solution {
 
 static void solve(double meal_cost, int tip_percent, int tax_percent) 
 {
